1079e67fc904f52fcc15e91c81f8bcb4a5a49c76
shubha-9051 <155102679+shubha-9051@users.noreply.github.com>
2024-01-17 16:49:41 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl.js')
+const app = express()
+
+mongoose.connect('mongodb+srv://shubhajit2003:<$hubha9051>@cluster0.wly4blk.mongodb.net/?retryWrites=true&w=majority')
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
+})
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
+
+  res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+})
+
+app.listen(3000);
